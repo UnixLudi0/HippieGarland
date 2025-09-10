af1713fdaa73b4a1e7100b917ec330ac92a6e747
flyofk <ggnekitos@gmail.com>
2025-09-10 22:39:59 +0000

    Fixed config.sh(configuration drivers)

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,57 +1,144 @@
-PS3="Конфигурация параметров..."
+PS3="Конфигурация параметров: "
 
 options=("Язык" "Процессор" "Видеокарта" "Выход")
 
-select opt in "${options[@]}"; do
-    case $opt in
-        "Язык")
-            options1=("Русский" "Английский")
-            select opt1 in "${options1[@]}"; do
-              case $opt1 in
-                "Русский")
-                  echo "ru" >> config.txt
+while true; do
+    echo "=== ГЛАВНОЕ МЕНЮ ==="
+    select opt in "${options[@]}"; do
+        case $opt in
+            "Язык")
+                options1=("Русский" "Английский")
+                echo "=== ВЫБОР ЯЗЫКА ==="
+                select opt1 in "${options1[@]}"; do
+                    case $opt1 in
+                        "Русский")
+                            echo "ru" >> config.txt
+                            echo "Выбран русский язык"
+                            break
+                            ;;
+                        "Английский")
+                            echo "us" >> config.txt
+                            echo "Выбран английский язык"
+                            break
+                            ;;
+                    esac
+                done
+                break
                 ;;
-                "Английский")
-                  echo "us" >> config.txt
+            "Процессор")
+                options2=("Intel" "AMD")
+                echo "=== ВЫБОР ПРОЦЕССОРА ==="
+                select opt2 in "${options2[@]}"; do
+                    case $opt2 in
+                        "Intel")
+                            echo "intel-cpu" >> config.txt
+                            echo "Выбран процессор Intel"
+                            break
+                            ;;
+                        "AMD")
+                            echo "amd-cpu" >> config.txt
+                            echo "Выбран процессор AMD"
+                            break
+                            ;;
+                    esac
+                done
+                break
                 ;;
-              esac
-            done
-            ;;
-        "Процессор")
-           options2=("Intel" "AMD")
-            select opt2 in "${options2[@]}"; do
-              case $opt2 in
-                "Intel")
-                  echo "intel-cpu" >> config.txt
+            "Видеокарта")
+                options3=("Intel" "AMD" "Nvidia")
+                echo "=== ВЫБОР ВИДЕОКАРТЫ ==="
+                select opt3 in "${options3[@]}"; do
+                    case $opt3 in
+                        "Intel")
+                            echo "intel-gpu" >> config.txt
+                            echo "Выбрана видеокарта Intel"
+                            break
+                            ;;
+                        "AMD")
+                            echo "amd-gpu" >> config.txt
+                            echo "Выбрана видеокарта AMD"
+                            break
+                            ;;
+                        "Nvidia")
+                            echo "nvidia-gpu" >> config.txt
+                            echo "Выбрана видеокарта Nvidia"
+                            break
+                            ;;
+                    esac
+                done
+                break
                 ;;
-                "AMD")
-                  echo "amd-cpu" >> config.txt
+            "Выход")
+                echo "Выход..."
+                exit 0
                 ;;
-              esac
-            done
-            ;;
-        "Видеокарта")
-           options3=("Intel" "AMD" "Nvidia")
-            select opt3 in "${options3[@]}"; do
-              case $opt3 in
-                "Intel")
-                  echo "intel-gpu" >> config.txt
+            *)
+                echo "Неверный выбор!"
+                break
                 ;;
-                "AMD")
-                  echo "amd-gpu" >> config.txt
-                ;;
-              "Nvidia")
-                echo "nvidia-gpu" >> config.txt
-              ;;
-              esac
-            done
-            ;;
-        "Выход")
-            echo "Выход..."
-            break
-            ;;
-        *)
-            echo "Неверный выбор!"
-            ;;
-    esac
+        esac
+    done
 done
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
